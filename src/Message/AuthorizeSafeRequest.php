5134b85a4b85241793648b1bc807d5bdbeafa94e
Timofey Zhiganov <flaszavod@gmail.com>
2020-10-14 13:58:26 +0300

    Extra amout берем из amount

--- src/Message/AuthorizeSafeRequest.php
+++ src/Message/AuthorizeSafeRequest.php
@@ -2,8 +2,6 @@
 
 namespace Omnipay\Best2Pay\Message;
 
-use Money\Formatter\DecimalMoneyFormatter;
-use Omnipay\Common\Message\AbstractRequest;
 
 /**
  * Class AuthorizeSafeRequest
@@ -11,55 +9,6 @@ use Omnipay\Common\Message\AbstractRequest;
  */
 class AuthorizeSafeRequest extends AuthorizeRequest
 {
-    public function getAmount()
-    {
-        $money = $this->getMoney();
-
-        if ($money !== null) {
-            $moneyFormatter = new DecimalMoneyFormatter($this->getCurrencies());
-
-            return $moneyFormatter->format($money);
-        }
-    }
-
-    /**
-     * Sets the payment amount.
-     *
-     * @param string|null $value
-     * @return $this
-     */
-    public function setAmount($value)
-    {
-        return $this->setParameter('amount', $value !== null ? (string) $value : null);
-    }
-    public function getServiceAmount(): int
-    {
-        return $this->getParameter('service_amount');
-    }
-
-    /**
-     * @param string $value
-     * @return $this
-     */
-    public function setServiceAmount(string $value): self
-    {
-        return $this->setParameter('service_amount', $value);
-    }
-
-    public function getExtraServiceAmount(): int
-    {
-        return $this->getParameter('extra_service_amount');
-    }
-
-    /**
-     * @param string $value
-     * @return $this
-     */
-    public function setExtraServiceAmount(string $value): self
-    {
-        return $this->setParameter('extra_service_amount', $value);
-    }
-
     public function getData(): array
     {
         $this->getCancelUrl();
@@ -79,10 +28,8 @@ class AuthorizeSafeRequest extends AuthorizeRequest
             'url' => $this->getReturnUrl(),
             'failUrl' => $this->getCancelUrl(),
             'get_token' => (int) $this->getGetToken(),
-            'service_amount' => $this->getServiceAmount(),
-            'extra_service_amount' => $this->getExtraServiceAmount(),
+            'extra_service_amount' => $this->getAmountInteger(),
         ];
-
 
         return $data;
     }
